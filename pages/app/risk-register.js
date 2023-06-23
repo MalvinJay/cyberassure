66b88fab38222961017df946017e4406bf0182bf
malvin_jay <arthurgeorgekojo@gmail.com>
2023-06-23 07:24:33 +0000

    Added risk-register drawer and removed risk-register page

--- pages/app/risk-register.js
+++ pages/app/risk-register.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { Button } from "antd";
-
-import AppLayout from "@/components/Layouts/appLayout";
-import AuthHead from "@/components/Misc/AuthHead";
-import DashboardFilter from "@/components/Misc/DashboardHead";
-
-import ExportEntity from "@/components/Misc/ExportEntity";
-import RegisterTable from "@/components/Misc/KRITable";
+import React, { useState, useEffect, useCallback } from "react";
+import dynamic from "next/dynamic";
+import { Drawer } from "antd";
 import api from "../../services/config";
 
-const RiskRegister = () => {
+import AppLayout from "@/components/Layouts/appLayout";
 
+const AuthHead = dynamic(() => import("@/components/Misc/AuthHead"))
+const DashboardFilter = dynamic(() => import("@/components/Misc/DashboardHead"))
+const ExportEntity = dynamic(() => import("@/components/Misc/ExportEntity"))
+const RegisterTable = dynamic(() => import("@/components/Misc/KRITable"))
+const AddEditRiskRegister = dynamic(() => import("@/components/RiskRegister/AddEditRiskRegister"))
+
+const RiskRegister = () => {
   const columns = [
     {
       title: "Risk Description ",
@@ -55,6 +55,7 @@ const RiskRegister = () => {
     },
   ];
 
+  const [open, setOpen] = useState(false);
   const [dataSource, setList] = useState([
     {
       description: "A vulnerable CRM Application Server on the internet",
@@ -89,6 +90,10 @@ const RiskRegister = () => {
     })
   };  
 
+  const onClose = useCallback(() => {
+    setOpen(false);
+  }, [])  
+
   useEffect(() => {
     fetchRiskRegisters();
   }, [])
@@ -101,51 +106,43 @@ const RiskRegister = () => {
 
         <div className="w-full pt-14 pb-20 px-8">
           <div className="flex justify-between items-center">
-            <Link href="/app/risk-register/new">
-              <button className="text-white bg-primary rounded-lg border-0 py-1 px-4 focus:outline-none hover:bg-primary/90 text-lg space-x-4 inline-flex items-center leading-tight">
-                <span>Create Risk <br /> Register</span>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
-                    fill="white"
-                  />
-                </svg>
-              </button>
-            </Link>
+            <button className="text-white bg-primary rounded-lg border-0 py-1 px-4 focus:outline-none hover:bg-primary/90 text-lg space-x-4 inline-flex items-center leading-tight"
+              onClick={() => setOpen(true)}
+            >
+              <span>Create Risk <br /> Register</span>
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 7H9V1C9 0.734784 8.89464 0.48043 8.70711 0.292893C8.51957 0.105357 8.26522 0 8 0C7.73478 0 7.48043 0.105357 7.29289 0.292893C7.10536 0.48043 7 0.734784 7 1V7H1C0.734784 7 0.48043 7.10536 0.292893 7.29289C0.105357 7.48043 0 7.73478 0 8C0 8.26522 0.105357 8.51957 0.292893 8.70711C0.48043 8.89464 0.734784 9 1 9H7V15C7 15.2652 7.10536 15.5196 7.29289 15.7071C7.48043 15.8946 7.73478 16 8 16C8.26522 16 8.51957 15.8946 8.70711 15.7071C8.89464 15.5196 9 15.2652 9 15V9H15C15.2652 9 15.5196 8.89464 15.7071 8.70711C15.8946 8.51957 16 8.26522 16 8C16 7.73478 15.8946 7.48043 15.7071 7.29289C15.5196 7.10536 15.2652 7 15 7Z"
+                  fill="white"
+                />
+              </svg>
+            </button>
 
             <h2 className="font-bold text-2xl ">Risk Register</h2>
 
             <ExportEntity name="Import CSV" />
           </div>
 
-          <div>
-            <div className="py-10">
-              <RegisterTable 
-                bordered={false}
-                customClass="risk-register" 
-                columns={columns}
-                dataSource={dataSource}
-                addRecord={false}
-              />
-
-              {/* <div className="pt-2 flex justify-end">
-                <Button
-                  type="primary"
-                  shape="default"
-                  className="bg-secondary text-white px-16 rounded-none font-bold text-base flex items-center shadow-inner"
-                >
-                  Save
-                </Button>
-              </div> */}
-            </div>
+          <div className="py-10">
+            <RegisterTable 
+              bordered={false}
+              customClass="risk-register" 
+              columns={columns}
+              dataSource={dataSource}
+              addRecord={false}
+            />
           </div>
         </div>
+
+        <Drawer width={670} title={<h3 className="text-3xl font-bold">Create Risk Register</h3>} placement="right" onClose={onClose} open={open}>
+          <AddEditRiskRegister kriType={2} onComplete={onClose} />
+        </Drawer>        
       </section>
     </AppLayout>
   );
